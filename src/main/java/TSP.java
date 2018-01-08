a703213174cb367c5d66f481b7caaa7ecb0008d4
ruhani-buet <1105059.rfr@ugrad.cse.buet.ac.bd>
2018-01-08 15:42:11 +0600

    Added MainClass.java to resolve ArrayIndexOutOfBoundException.

--- src/main/java/TSP.java
+++ src/main/java/TSP.java
@@ -22,36 +22,31 @@ public class TSP
 	public TSP(double x[], double y[], int n) 
 	{
 		// TODO Auto-generated constructor stub
-		this.x = x;
-		this.y = y;
+		this.x = x.clone();
+		this.y = y.clone();
 		this.n = n;
 
 	}
 
-	TSP() {
-		this.x = new double[10];
-		this.y = new double[10];
-		this.n = 10;
-	}
-	
-	private void solve() {
+	public void solve() {
 		Route route = new Route(this);
 		//route.print();
-		
+
 
 		SimulatedAnnealing simulatedAnnealing = new SimulatedAnnealing(this, 24, 0.6, 0.3);
 		SolutionInfo si = simulatedAnnealing.run();
-		
+
 		System.out.println(si.solution.getCost());
 		si.solution.print();
 	}
-	void print()
+
+	public void print()
 	{
 		System.out.println("Dimension: "+n);
 		for (int i = 0; i < n; i++) {
-			System.out.println(x[i] + " " + y[i]);
+			System.out.println(x[i] + "\t" + y[i]);
 		}
-                System.out.println("");
+		System.out.println("");
 	}
 	
 	/**
@@ -67,25 +62,22 @@ public class TSP
             return d;
 	}
 
-	public static void main(String[] args) {
-		new TSP().processInput();
-	}
-	public void processInput()
+	public static void main(String[] args)
 	{
-		try 
+		try
 		{
-			String inputFileName = "/input/test.tsp";
-			InputStream inputStream = this.getClass().getResourceAsStream(inputFileName);
-			Scanner in = new Scanner(inputStream);
+//			String inputFileName = "/input/burma14.tsp";
+//			InputStream inputStream = this.getClass().getResourceAsStream(inputFileName);
+//			Scanner in = new Scanner(inputStream);
 			//Scanner in = new Scanner(new File("H:\\Level 4 Term 2\\CSE 462 Algorithm Engineering Sessional\\Offline 1 TSP\\Starts\\att48.tsp"));
-//			Scanner in = new Scanner(new File("\\input\\burma14.tsp"));
+            Scanner in = new Scanner(new File("src\\main\\resources\\input\\burma14.tsp"));
 			//Scanner in = new Scanner(new File("st70.tsp"));
 			//Scanner in = new Scanner(new File("ulysses16.tsp"));
 			//Scanner in = new Scanner(new File("ulysses22.tsp"));
-			
+
 			String line = "";
 			int n;
-			
+
 			//three comment lines
 			in.nextLine();
 			in.nextLine();
@@ -94,32 +86,39 @@ public class TSP
 			line = in.nextLine();
 			line = line.substring(11).trim();
 			n = Integer.parseInt(line);
-			
+
 			//System.out.println("" +n);
-			
+
 			//two comment lines
 			in.nextLine();
 			in.nextLine();
-			
+
 			double x[] = new double[n];
 			double y[] = new double[n];
-			
+
 			for (int i = 0; i < n; i++)
 			{
 				in.nextInt();
 				x[i] = in.nextDouble();
 				y[i] = in.nextDouble();
 			}
-			
+
+			for (int i = 0; i<x.length; i++) {
+				System.out.println(x[i]);
+			}
+			System.out.println(x.length);
+			System.out.println(n);
 			TSP tsp = new TSP (x,y,n);
-			//tsp.print();
+			tsp.print();
 			tsp.solve();
-		} 
-		catch (Exception e) 
+		}
+		catch (Exception e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-		
+		}
+
 	}
+
+
 }
